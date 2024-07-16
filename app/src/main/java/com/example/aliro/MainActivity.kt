f816e9e3a893ec9fde655277d29792743355df25
Himanshi Agarwal <himanshiagarwal1408@gmail.com>
2024-07-16 10:35:08 +0530

    Connected Splash and Login Page - Himanshi

--- app/src/main/java/com/example/aliro/MainActivity.kt
+++ app/src/main/java/com/example/aliro/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.aliro
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,7 +11,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.login)
+        setContentView(R.layout.splash)
+
+        val button = findViewById<Button>(R.id.proceed_button)
+        button?.setOnClickListener(){
+            setContentView(R.layout.login)
+        }
 
     }
 }
