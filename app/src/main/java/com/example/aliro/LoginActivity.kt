76047ca29526883a9d8518e59732d43b4e9282d2
Gaurav Jain <gauravjain0781@gmail.com>
2024-07-28 11:44:09 +0530

    Updated Login SignUp and Sidebar - Gaurav

--- app/src/main/java/com/example/aliro/LoginActivity.kt
+++ app/src/main/java/com/example/aliro/LoginActivity.kt
@@ -3,6 +3,7 @@ package com.example.aliro
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
@@ -13,6 +14,11 @@ class LoginActivity : AppCompatActivity() {
         setContentView(R.layout.login)
 
         val loginButton = findViewById<Button>(R.id.login_button)
+        val signUpTextView = findViewById<TextView>(R.id.signupLink)
+
+        signUpTextView.setOnClickListener(){
+            startActivity(Intent(this, SignUpActivity:: class.java))
+        }
 
         loginButton?.setOnClickListener(){
             startActivity(Intent(this, HomeActivity::class.java))
