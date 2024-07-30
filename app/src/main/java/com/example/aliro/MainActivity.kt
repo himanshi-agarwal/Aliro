ad072ca2a0a897d97466684cfa0c131a4bd7f38e
Himanshi Agarwal <himanshiagarwal1408@gmail.com>
2024-07-30 10:50:37 +0530

    Splash and Visitor Diary Update - Himanshi

--- app/src/main/java/com/example/aliro/MainActivity.kt
+++ app/src/main/java/com/example/aliro/MainActivity.kt
@@ -2,20 +2,22 @@ package com.example.aliro
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private val SPLASH_SCREEN_TIME: Long = 2000
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.splash)
 
-        val button = findViewById<Button>(R.id.proceed_button)
-        button?.setOnClickListener(){
+        Handler(Looper.getMainLooper()).postDelayed({
             startActivity(Intent(this, LoginActivity::class.java))
-        }
-
+            finish()
+        }, SPLASH_SCREEN_TIME)
     }
 }
