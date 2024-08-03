84205c1c72c6d5dcac05528577a6f4f134c2ec2c
Gaurav Jain <gauravjain0781@gmail.com>
2024-08-03 08:10:50 +0530

    Database Design - Gaurav

--- app/src/main/java/com/example/aliro/LoginActivity.kt
+++ app/src/main/java/com/example/aliro/LoginActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
