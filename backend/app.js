995e3d29c2f545574c4aa1db57b4041ed01a6d8a
Gaurav Jain <gauravjain0781@gmail.com>
2024-08-20 07:55:48 +0530

    Backend in MongoDB Express Node

---
+++ backend/app.js
@@ -0,0 +1,23 @@
+import express from "express";
+import dotenv from "dotenv";
+import userRoutes from './routes/userRoutes.js'
+import connectDB from "./config/db.js";
+
+dotenv.config()
+connectDB()
+
+const port = process.env.PORT
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true}))
+
+app.get('/', (req, res) => {
+    res.send("Server Running")
+})
+
+app.use('/user', userRoutes)
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`)
+})
