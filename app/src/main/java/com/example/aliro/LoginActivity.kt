5a8f27ac26e9383256370946cc33c6a8b740294f
Himanshi Agarwal <himanshiagarwal1408@gmail.com>
2024-09-03 21:59:53 +0530

    Login Page db - Himanshi

--- app/src/main/java/com/example/aliro/LoginActivity.kt
+++ app/src/main/java/com/example/aliro/LoginActivity.kt
@@ -2,18 +2,23 @@ package com.example.aliro
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.firestore
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var username : EditText
     private lateinit var password : EditText
     private lateinit var loginButton : Button
     private lateinit var signUpTextView : TextView
+
+    private var db = Firebase.firestore
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -47,5 +52,35 @@ class LoginActivity : AppCompatActivity() {
     }
 
     private fun fetchData(name: String, password: String) {
+        db.collection("users")
+            .whereEqualTo("name", name)
+            .whereEqualTo("password", password)
+            .get()
+            .addOnSuccessListener { document ->
+                Log.d("data", document)
+                if(document.isEmpty){
+                    Toast.makeText(this, "Invalid Credentials", Toast.LENGTH_SHORT).show()
+                }
+                else {
+                    for (d in document) {
+                        val userName = d.getString("name")
+                        val userType = d.getString("user_type")
+                        val intent:Intent
+                        Toast.makeText(this, "Welcome, $userName", Toast.LENGTH_SHORT).show()
+
+                        if (userType == "Employee"){
+                            intent = Intent (this, EmpHomeActivity::class.java)
+                        }
+                        else{
+                            intent = Intent (this, VisitorHomeActivity::class.java)
+                        }
+                        startActivity(intent)
+                        finish()
+                    }
+                }
+            }
+            .addOnFailureListener { exception ->
+                Toast.makeText(this, "Login Failed! Try Again..", Toast.LENGTH_SHORT).show()
+            }
     }
 }
