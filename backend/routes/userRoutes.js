c8a7a16f1317a45c9778468862985b21a56e8927
Gaurav Jain <gauravjain0781@gmail.com>
2024-09-06 17:39:05 +0530

    Visitor Home Page - Gaurav

--- backend/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express"
-import { getUsers, authLogin } from '../controller/userController.js'
-
-const router = express.Router()
-
-router.route('/').get(getUsers)
-router.route('/').post(authLogin)
-
-export default router
