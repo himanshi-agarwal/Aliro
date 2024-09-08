14fd21afdc8f98b742085abca1013996765ba4ca
Gaurav Jain <gauravjain0781@gmail.com>
2024-09-08 13:26:37 +0530

    Splash Screen Animation - Gaurav

--- app/src/main/java/com/example/aliro/MainActivity.kt
+++ app/src/main/java/com/example/aliro/MainActivity.kt
@@ -4,19 +4,38 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
 
 class MainActivity : AppCompatActivity() {
-    private val SPLASH_SCREEN_TIME: Long = 2000
+    private lateinit var topAnimation : Animation
+    private lateinit var bottomAnimation : Animation
+    private lateinit var logo : CardView
+    private lateinit var app : TextView
+    private lateinit var slogan : TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.splash)
 
+        topAnimation = AnimationUtils.loadAnimation(this, R.anim.top_animation)
+        bottomAnimation = AnimationUtils.loadAnimation(this, R.anim.bottom_animation)
+
+        logo = findViewById(R.id.logo)
+        app = findViewById(R.id.app_name)
+        slogan = findViewById(R.id.slogan)
+
+        logo.startAnimation(topAnimation)
+        app.startAnimation(bottomAnimation)
+        slogan.startAnimation(bottomAnimation)
+
         Handler(Looper.getMainLooper()).postDelayed({
             startActivity(Intent(this, LoginActivity::class.java))
             finish()
-        }, SPLASH_SCREEN_TIME)
+        }, 3000)
     }
 }
