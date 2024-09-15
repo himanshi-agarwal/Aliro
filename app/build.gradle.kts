e9bf099ae92f546d343e68f467ad2858ea235351
Gaurav Jain <gauravjain0781@gmail.com>
2024-09-15 21:37:02 +0530

    Profile Picture Saved and Reterive - Gaurav

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,13 +37,14 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.firebase.firestore)
+    implementation(libs.firebase.storage)
+    implementation("com.github.bumptech.glide:glide:4.16.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
