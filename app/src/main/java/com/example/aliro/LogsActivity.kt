dc4b9a66bcde6895f970bd03834f8477c61801b1
Gaurav Jain <gauravjain0781@gmail.com>
2024-09-18 00:13:15 +0530

    Logs Backend - Gaurav

--- app/src/main/java/com/example/aliro/LogsActivity.kt
+++ app/src/main/java/com/example/aliro/LogsActivity.kt
@@ -1,23 +1,45 @@
 package com.example.aliro
 
 import android.content.Intent
+import android.graphics.Typeface
 import android.os.Bundle
+import android.util.Log
+import android.util.TypedValue
+import android.view.Gravity
 import android.view.Menu
 import android.view.MenuItem
+import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TableLayout
+import android.widget.TableRow
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
+import androidx.core.content.res.ResourcesCompat
 import androidx.core.view.GravityCompat
+import androidx.core.view.setMargins
+import androidx.core.view.setPadding
 import androidx.drawerlayout.widget.DrawerLayout
 import com.bumptech.glide.Glide
 import com.google.android.material.navigation.NavigationView
 import com.google.firebase.Firebase
+import com.google.firebase.Timestamp
 import com.google.firebase.firestore.firestore
 import com.google.firebase.storage.storage
+import java.text.SimpleDateFormat
+import java.util.Locale
+import java.util.TimeZone
+
+data class LogEntry(
+    val inTime: Timestamp? = null,
+    val outTime: Timestamp? = null,
+    val duration: String = "",
+)
 
 class LogsActivity: AppCompatActivity() {
     private lateinit var drawerToggle: ActionBarDrawerToggle
@@ -67,11 +89,11 @@ class LogsActivity: AppCompatActivity() {
                 empCompany.text = employee[4]
 
                 updateSidebarHeader()
+                fetchLogsByUser()
             } else {
                 Toast.makeText(this, "Failed to load user data", Toast.LENGTH_SHORT).show()
             }
         }
-
 
         toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
@@ -203,6 +225,151 @@ class LogsActivity: AppCompatActivity() {
     private fun checkSession() : Boolean{
         val sharedPreference = getSharedPreferences("user_session", MODE_PRIVATE)
         return sharedPreference.getBoolean("loggedIn", false)
+    }
+
+    private fun fetchLogsByUser() {
+        if(checkSession()){
+            val sharedPreference = getSharedPreferences("user_session", MODE_PRIVATE)
+            val userId = sharedPreference.getString("userId", null)
+
+            val db = Firebase.firestore
+            val userRef = db.collection("user").document(userId!!)
+
+            db.collection("logs")
+                .whereEqualTo("user_ref", userRef)
+                .get()
+                .addOnSuccessListener { documents ->
+                    val logsByDate = mutableMapOf<String, MutableList<LogEntry>>()
+
+                    val dateFormatter = SimpleDateFormat("dd MMMM yyyy", Locale.getDefault())
+                    dateFormatter.timeZone = TimeZone.getTimeZone("Asia/Kolkata")
+
+                    for (document in documents) {
+                        val log = document.toObject(LogEntry::class.java)
+                        val logDateFormatted = log.inTime?.let { dateFormatter.format(it.toDate()) } ?: "Unknown Date"
+
+                        val formattedLog = LogEntry(
+                            inTime = log.inTime,
+                            outTime = log.outTime,
+                            duration = calculateDuration(log.inTime, log.outTime)
+                        )
+
+                        Log.i("LOG", formattedLog.toString())
+                        logsByDate.getOrPut(logDateFormatted) { mutableListOf() }.add(formattedLog)
+                    }
+
+                    displayLogsByDate(logsByDate)
+                }
+                .addOnFailureListener { exception ->
+                    Toast.makeText(this, "Error in Logs", Toast.LENGTH_SHORT).show()
+                    Log.w("Firestore", "Error getting documents: ", exception)
+                }
+        } else {
+            Toast.makeText(this, "Error in User Login", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun displayLogsByDate(logsByDate: Map<String, List<LogEntry>>) {
+        val parentLayout = findViewById<LinearLayout>(R.id.tableparent)
+        parentLayout.removeAllViews()
+
+        val datePadding = resources.getDimensionPixelSize(R.dimen.date_padding)
+        val tablePadding = resources.getDimensionPixelSize(R.dimen.table_padding)
+        val tableMargin = resources.getDimensionPixelSize(R.dimen.table_margin)
+        val rowPadding = resources.getDimensionPixelSize(R.dimen.table_row_padding)
+
+        for ((date, logs) in logsByDate) {
+            val dateTextView = TextView(this).apply {
+                text = "Date: ${date}"
+                setPadding(datePadding, datePadding, datePadding, datePadding)
+                gravity = Gravity.START
+                setTextColor(ContextCompat.getColor(context, R.color.black))
+                typeface = ResourcesCompat.getFont(context, R.font.lato_italic)
+                setTypeface(typeface, Typeface.BOLD)
+            }
+            parentLayout.addView(dateTextView)
+
+            val tableLayout = TableLayout(this).apply {
+                layoutParams = LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.MATCH_PARENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT
+                ).apply {
+                    setMargins(tableMargin, 0, tableMargin, 0)
+                }
+                setPadding(tablePadding, tablePadding, tablePadding, tablePadding)
+                gravity = Gravity.START
+                isStretchAllColumns = true
+                isShrinkAllColumns = true
+                background = ContextCompat.getDrawable(context, R.drawable.rounded_corner)
+                backgroundTintList = ContextCompat.getColorStateList(context, R.color.white)
+            }
+
+            val headerRow = TableRow(this).apply {
+                addView(createHeaderTextView("In Time"))
+                addView(createHeaderTextView("Out Time"))
+                addView(createHeaderTextView("Duration"))
+            }
+            tableLayout.addView(headerRow)
+
+            for (log in logs) {
+                val tableRow = TableRow(this).apply {
+                    setPadding(rowPadding, rowPadding, rowPadding, rowPadding)
+                    setBackgroundColor(ContextCompat.getColor(context, R.color.white))
+                }
+
+                tableRow.addView(createTextView(formatTimestampToTimeString(log.inTime)))
+                tableRow.addView(createTextView(formatTimestampToTimeString(log.outTime)))
+                tableRow.addView(createTextView(log.duration))
+
+                tableLayout.addView(tableRow)
+            }
+
+            parentLayout.addView(tableLayout)
+        }
+    }
+
+    private fun createTextView(text: String): TextView {
+        val textPadding = resources.getDimensionPixelSize(R.dimen.text_padding)
+        return TextView(this).apply {
+            this.text = text
+            gravity = Gravity.CENTER
+            setPadding(textPadding, textPadding, textPadding, textPadding)
+        }
+    }
+
+    private fun createHeaderTextView(text: String): TextView {
+        val textPadding = resources.getDimensionPixelSize(R.dimen.text_padding)
+        return TextView(this).apply {
+            this.text = text
+            gravity = Gravity.CENTER
+            setPadding(textPadding, textPadding, textPadding, textPadding)
+            setTextColor(ContextCompat.getColor(context, R.color.black))
+            setTypeface(typeface, Typeface.BOLD)
+        }
+    }
+
+    private fun formatTimestampToTimeString(timestamp: Timestamp?): String {
+        return timestamp?.let {
+            val sdf = SimpleDateFormat("HH:mm:ss", Locale.getDefault())
+            sdf.format(it.toDate())
+        } ?: "Unknown"
+    }
+
+    private fun calculateDuration(inTime: Timestamp?, outTime: Timestamp?): String {
+        if (inTime == null || outTime == null) {
+            return "Invalid times"
+        }
+
+        val inDate = inTime.toDate()
+        val outDate = outTime.toDate()
+
+        val durationMillis = outDate.time - inDate.time
+
+        val durationSeconds = durationMillis / 1000
+        val minutes = (durationSeconds % 3600) / 60
+        val seconds = durationSeconds % 60
+
+        return String.format("%02d:%02d", minutes, seconds)
     }
 
     private fun logout() {
